da9562e2c600dc1717d9b31ed66e144cf9d833d9
Abhijith <abhijithpb2506@gmail.com>
2025-12-18 17:14:56 +0530

    Add search component withy city search functionality,Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+
+import './App.css'
+import Itinerary from './Components/Itinerary'
+import NavBar from './Components/NavBar'
+import SearchBar from './Components/SearchBar'
+import TripProvider from './Context/TripProvider'
 function App() {
+  
   return (
-    <>
-     
-    
-    </>
+    <TripProvider>
+      <SearchBar/>
+      <Itinerary/>
+      <NavBar/>
+    </TripProvider>
   )
 }
 
