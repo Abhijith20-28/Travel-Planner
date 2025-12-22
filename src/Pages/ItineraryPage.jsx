3b795cf0ca9c0bc6c3b7f01654d8dcc3a65120af
Abhijith <abhijithpb2506@gmail.com>
2025-12-22 23:18:10 +0530

    implement itinerary page with itinerary list and city modal

---
+++ src/Pages/ItineraryPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Itinerary from '../Components/Itinerary'
+import ModalCity from '../Modal/ModalCity'
+
+function ItineraryPage() {
+  return (
+    <div>
+      <Itinerary/>
+      <ModalCity/>
+    </div>
+  )
+}
+
+export default ItineraryPage
