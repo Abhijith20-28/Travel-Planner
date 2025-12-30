39cf83aafcac7f901fe3752a4786c2bbc8060f69
Abhijith <abhijithpb2506@gmail.com>
2025-12-30 12:06:09 +0530

    add useAccomodation to fetch city accommodation from firestore

---
+++ src/hooks/useAccomodations.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+
+export function useAccomodations(cityId) {
+  const [accomodations, setAccomodations] = useState([]);
+
+  useEffect(() => {
+    if(!cityId) return; 
+    async function fetchAccomodations() {
+      const snapshot = await getDocs(collection(db,"cities",cityId,"accomodations"));
+     
+      const accomodations = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+     
+    setAccomodations(accomodations)
+    }
+    fetchAccomodations();
+    
+  }, [cityId]);
+
+  return accomodations;
+}
