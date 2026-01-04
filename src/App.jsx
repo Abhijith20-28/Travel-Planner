8c9132c1d2cf17ae497187046031cf6eaa8d0076
Abhijith <abhijithpb2506@gmail.com>
2026-01-04 11:38:24 +0530

    Add Route to navigate pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-
 import './App.css'
 import Itinerary from './Components/Itinerary'
 import NavBar from './Components/NavBar'
 import SearchBar from './Components/SearchBar'
+import CityList from './Components/CityList'
 import TripProvider from './Context/TripProvider'
+import Hero from './Pages/Hero'
+import { Route,Routes,Outlet} from 'react-router-dom'
+import SearchPage from './Pages/SearchPage'
+import ItineraryPage from './Pages/ItineraryPage'
+
+function LayoutWithNav() {
+  return (
+    <>
+      <NavBar />
+      <Outlet/>
+    </>
+  )
+}
 function App() {
-  
   return (
     <TripProvider>
-      <SearchBar/>
-      <Itinerary/>
-      <NavBar/>
+        <Routes>
+          <Route path='/' element={<Hero/>}/>
+          <Route element={<LayoutWithNav/>}>
+          <Route path='/search' element={<SearchPage/>}/>
+          <Route path='/itinerary' element={<ItineraryPage/>}/>
+          </Route>
+        </Routes>
     </TripProvider>
-  )
+  );
 }
 
 export default App
